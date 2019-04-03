db21990fd903191562576613cfc0b7cfd4cf8b18
Vasanth <vasanth3g@gmail.com>
2019-04-03 18:32:47 +0530

    Update remap.sh

--- configs/remap.sh
+++ configs/remap.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "map http://$HOST:$PORT1 http://$ORIGIN_HOST1 plugin=header_rewrite.so @pparam=dscp/set_dscp_32.config  @plugin=header_rewrite.so @pparam=hdr_rw_live.config @plugin=collapsed_forwarding.so @plugin=cache_range_requests.so" > /opt/trafficserver/etc/trafficserver/remap.config 
+HOST=`hostname`
+###LIVE
+
+echo "map http://$HOST:$LIVE_PORT http://$LIVE_ORIGIN plugin=header_rewrite.so @pparam=dscp/set_dscp_32.config  @plugin=header_rewrite.so @pparam=hdr_rw_live.config @plugin=collapsed_forwarding.so @plugin=cache_range_requests.so" > /opt/trafficserver/etc/trafficserver/remap.config 
+
+
+### VOD
+
+echo "map http://$HOST:$VOD_PORT http://$VOD_ORIGIN plugin=header_rewrite.so @pparam=dscp/set_dscp_32.config  @plugin=header_rewrite.so @pparam=header_vod.config @plugin=collapsed_forwarding.so @plugin=cache_range_requests.so" >> /opt/trafficserver/etc/trafficserver/remap.config 
+
+##### Start Trafficserver ############
+
 /opt/trafficserver/bin/traffic_server
