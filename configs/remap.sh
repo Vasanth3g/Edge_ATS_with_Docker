d7d0b3de16ca1a8db83c8224c01fd486d3f6ee76
Vasanth <vasanth3g@gmail.com>
2019-04-03 19:32:44 +0530

    Update remap.sh

--- configs/remap.sh
+++ configs/remap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONFIG=$(sed -e 's/CONFIG proxy.config.http.server_ports STRING *..*/CONFIG proxy.config.http.server_ports STRING $LIVE_PORT $VOD_PORT/g')
+sed -i 's/CONFIG proxy.config.http.server_ports STRING *..*/CONFIG proxy.config.http.server_ports STRING $LIVE_PORT $VOD_PORT/g' /opt/trafficserver/etc/trafficserver/records.config
 HOST=`hostname`
 ###LIVE
 
