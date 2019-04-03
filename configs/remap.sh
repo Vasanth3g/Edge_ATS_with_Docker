829ce615fa9f300080bbc42b66c4aaab5abf4107
Vasanth <vasanth3g@gmail.com>
2019-04-03 18:19:41 +0530

    Update remap.sh

--- configs/remap.sh
+++ configs/remap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "map http://$HOST:$PORT1 http://$ORIGIN_HOST1" > /opt/trafficserver/etc/trafficserver/remap.config 
+echo "map http://$HOST:$PORT1 http://$ORIGIN_HOST1 plugin=header_rewrite.so @pparam=dscp/set_dscp_32.config  @plugin=header_rewrite.so @pparam=hdr_rw_live.config @plugin=collapsed_forwarding.so @plugin=cache_range_requests.so" > /opt/trafficserver/etc/trafficserver/remap.config 
 /opt/trafficserver/bin/traffic_server
