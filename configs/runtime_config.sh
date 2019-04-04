bcf13ddb1131de414ad0860698d6e96983fb2408
Vasanth <vasanth3g@gmail.com>
2019-04-04 12:39:18 +0530

    Updated hosting config

--- configs/runtime_config.sh
+++ configs/runtime_config.sh
@@ -2,7 +2,8 @@
 sed -i 's/CONFIG proxy.config.http.server_ports STRING *..*/CONFIG proxy.config.http.server_ports STRING '$LIVE_PORT' '$VOD_PORT'/g' /opt/trafficserver/etc/trafficserver/records.config
 HOST=$(ifconfig | grep -w inet | awk '{print$2}' | awk -F : 'NR==1{print$2}')
 #HOST=`hostname`
-echo "hostname=$VOD_ORIGIN volume=1 \nhostname=$LIVE_ORIGIN volume=2" > /opt/trafficserver/etc/trafficserver/hosting.config
+echo "hostname=$VOD_ORIGIN volume=1
+hostname=$LIVE_ORIGIN volume=2" > /opt/trafficserver/etc/trafficserver/hosting.config
 ###LIVE
 
 echo "map http://$HOST:$LIVE_PORT http://$LIVE_ORIGIN plugin=header_rewrite.so @pparam=dscp/set_dscp_32.config  @plugin=header_rewrite.so @pparam=hdr_rw_live.config @plugin=collapsed_forwarding.so @plugin=cache_range_requests.so" > /opt/trafficserver/etc/trafficserver/remap.config 
